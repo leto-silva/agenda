dc0f173f8cb3dfb52dde9b3442f1c5319fe676ac
WellingtonEugenio <wellingtoneugenio@gmail.com>
2024-04-26 17:02:26 -0300

    create php iniciado

--- templates/header.php
+++ templates/header.php
@@ -21,8 +21,14 @@ include_once('config/url.php');
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="<?= $BASE_URL ?>/index.php">
-                <img src="<?= $BASE_URL ?>/img/logo.svg" alt="Agenda">
-            </a>    
+            <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
+                <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
+            </a>
+            <div>
+                <div class="navbar-nav">
+                    <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>index.php">Agenda</a>
+                    <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>create.php">Adicionar Contato</a>
+                </div>    
+            </div>        
         </nav>    
     </header>
